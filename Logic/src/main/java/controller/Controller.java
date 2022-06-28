17481e35d2300b7e3e20fd0e30f0c44f28846cb9
Lennart Protte <lenni.protte@gmx.de>
2022-06-28 16:02:39 +0200

    Controller.java

--- Logic/src/main/java/controller/Controller.java
+++ Logic/src/main/java/controller/Controller.java
@@ -1,4 +1,31 @@
 package controller;
 
-public class Controller {
+import domain.Field;
+import utils.Generator;
+import utils.GridUtils;
+import utils.Solver;
+
+public class Controller extends GridUtils {
+    private int[][] grid;
+    private Generator generator;
+    private Solver solver;
+    public Controller(int difficulty) {
+        this.grid = generatePuzzle(difficulty);
+    }
+
+    private int[][] generatePuzzle(int difficulty) {
+        return null;
+    }
+
+    public boolean isSolved() {
+        return new Solver(toComparableGrid(grid)).isSolved();
+    }
+
+    public int[][] getGrid() {
+        return null;
+    }
+
+    public int[][] solveGrid() {
+        return null;
+    }
 }
